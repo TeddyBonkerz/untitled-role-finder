53bc8251dc58e113c1a1b62867c29ba7f2d98f15
TeddyBonkerz <33665345+TeddyBonkerz@users.noreply.github.com>
2025-01-05 17:00:49 -0500

    PA: setup env properties.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Import packages
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+// Populate values from your OpenAI deployment
+var modelId = configuration[OpenAI:ModelId];
+var endpoint = configuration[OpenAI:Endpoint];
+var apiKey = configuration[OpenAI:ApiKey];
+
+// Create a kernel with Azure OpenAI chat completion
+var builder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// Add enterprise components
+builder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+// Build the kernel
+Kernel kernel = builder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Add a plugin (the LightsPlugin class is defined below)
+kernel.Plugins.AddFromType<LightsPlugin>("Lights");
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+// Initiate a back-and-forth chat
+string? userInput;
+do {
+    // Collect user input
+    Console.Write("User > ");
+    userInput = Console.ReadLine();
+
+    // Add user input
+    history.AddUserMessage(userInput);
+
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+
+    // Print the results
+    Console.WriteLine("Assistant > " + result);
+
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+} while (userInput is not null);
